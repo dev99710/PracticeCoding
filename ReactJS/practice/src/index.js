56535514ab7d3101e159c4d900c22cb585cb5679
DEV CHAVDA <123221819+dev99710@users.noreply.github.com>
2025-01-24 14:14:22 +0000

    Counter

--- ReactJS/practice/src/index.js
+++ ReactJS/practice/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import TodoApp from './Todo';
 import reportWebVitals from './reportWebVitals';
+import Counter from './Counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TodoApp />
+    <Counter />
   </React.StrictMode>
 );
 
